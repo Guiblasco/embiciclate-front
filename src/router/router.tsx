7eb65acd245379a78e7a3b9c6fa8047c2738506d
Guillem Blasco <gowet92@gmail.com>
2024-09-07 16:18:57 +0200

    Add components on router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,7 @@
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  Navigate,
   Route,
 } from "react-router-dom";
 import App from "../components/App/App";
@@ -9,6 +10,7 @@ import { NotFoundPage } from "../pages/NotFoundPage";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
+      <Route index element={<Navigate to="/bicis" />} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>,
   ),
