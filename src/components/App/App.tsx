869e41138e44bbd8f34f761faef1938b9724ef72
Guillem Blasco <gowet92@gmail.com>
2024-09-07 16:49:49 +0200

    Add Outlet component at App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "../Header/Header";
+import { Outlet } from "react-router-dom";
 
 const App = (): React.ReactElement => {
   return (
     <div className="main-container">
       <Header />
+      <Outlet />
     </div>
   );
 };
