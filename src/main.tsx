9e6bc7dddac1fe250126b0374dc48906e8950789
Guillem Blasco <gowet92@gmail.com>
2024-09-07 14:35:14 +0200

    Add router component

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App/App";
 import "@fontsource/league-spartan/400.css";
 import "@fontsource/league-spartan/600.css";
 import "./styles/styles.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
