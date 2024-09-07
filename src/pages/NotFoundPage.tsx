40a3d1ade16fdc4cd5bf52486dce0ad329c75908
Guillem Blasco <gowet92@gmail.com>
2024-09-07 13:46:42 +0200

    Add NotFoundPage component

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./NotFoundPage.css";
+
+export const NotFoundPage = (): React.ReactElement => {
+  return (
+    <div className="error-container">
+      <h2 className="error-container__title">La página no existe</h2>
+      <img
+        src="media/bici-rota-404.webp"
+        alt="Una bici rota"
+        width={330}
+        height={177}
+      ></img>
+    </div>
+  );
+};
