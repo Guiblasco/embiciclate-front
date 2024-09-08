44eca1cdca2989f2ec3ae6b199a116db8ad2a762
Guillem Blasco <gowet92@gmail.com>
2024-09-08 12:58:12 +0200

    Add BikeFormPage at router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -7,12 +7,14 @@ import {
 import App from "../components/App/App";
 import { NotFoundPage } from "../pages/NotFoundPage";
 import BikesListPage from "../components/bike/pages/BikesListPage/BikesListPage";
+import BikesFormPage from "../components/bike/pages/BikeFormPage/BikeFormPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Navigate to="/bikes" />} />
       <Route path="bikes" element={<BikesListPage />} />
+      <Route path="add" element={<BikesFormPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>,
   ),
