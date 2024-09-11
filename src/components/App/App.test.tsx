5c089088434a064240796e9d736127aea850e634
Guillem Blasco <gowet92@gmail.com>
2024-09-11 15:57:14 +0200

    Refactor 'Then' from App test

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -5,7 +5,7 @@ import router from "../../router/router";
 
 describe("Given the App component", () => {
   describe("When it is rendered", () => {
-    test("Then it should be redirected to the path '/bikes' and show 'Lista de bicis' in a heading", () => {
+    test("Then it should be show 'Lista de bicis' in a heading", () => {
       const subtitleRegExp = new RegExp(/lista de bicis/, "i");
 
       render(<RouterProvider router={router} />);
@@ -17,7 +17,7 @@ describe("Given the App component", () => {
   });
 
   describe("When user clicks in 'Añadir'", () => {
-    test("Then it should be redirected to the path '/add' and show 'Crear una bici' in a heading", async () => {
+    test("Then it should be show 'Crear una bici' in a heading", async () => {
       const user = userEvent.setup();
       const addRegExp = new RegExp(/añadir/, "i");
       const bikeFormPageRegExp = new RegExp(/crear una bici/, "i");
