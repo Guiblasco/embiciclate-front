283325c395605450e058d22e9ffef90d3cf6b3d1
Guillem Blasco <gowet92@gmail.com>
2024-09-13 18:55:45 +0200

    Add fetch function on BikeListPage component

--- src/bike/pages/BikesListPage/BikesListPage.tsx
+++ src/bike/pages/BikesListPage/BikesListPage.tsx
@@ -1,9 +1,27 @@
+import { useEffect, useMemo, useState } from "react";
 import "./BikesListPage.css";
+import { BikeClient } from "../../api/BikesClient";
+import { Bike } from "../../types";
+import BikeList from "../../components/BikeList/BikeList";
 
 const BikesListPage = (): React.ReactElement => {
+  const bikeClient = useMemo(() => new BikeClient(), []);
+
+  const [bikes, setBikes] = useState<Bike[]>([]);
+
+  useEffect(() => {
+    const fetchbikes = async () => {
+      const apiBikes = await bikeClient.getBikes();
+      setBikes(apiBikes);
+    };
+
+    fetchbikes();
+  }, [bikeClient]);
+
   return (
     <main className="page-container">
       <h1 className="page-container__title">Lista de bicis</h1>
+      <BikeList bikes={bikes} />
     </main>
   );
 };
