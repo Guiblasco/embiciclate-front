85f3b4f7a0d2ec4d6c46b278e0f8fbdd0886dfbe
Guillem Blasco <gowet92@gmail.com>
2024-09-15 18:14:31 +0200

    Remove coments

--- src/components/Toasts/Toast.tsx
+++ src/components/Toasts/Toast.tsx
@@ -1,8 +1,5 @@
 import { ToastContainer, toast } from "react-toastify";
-
 import "react-toastify/dist/ReactToastify.css";
-// minified version is also included
-// import 'react-toastify/dist/ReactToastify.min.css';
 
 function Toast() {
   const notify = () => {
