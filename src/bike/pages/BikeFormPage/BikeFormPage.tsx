6285b8d278932f880aaf3d685b499cfdff4428c5
Guillem Blasco <gowet92@gmail.com>
2024-09-22 16:19:57 +0200

    Add function that calls client to add a bike

--- src/bike/pages/BikeFormPage/BikeFormPage.tsx
+++ src/bike/pages/BikeFormPage/BikeFormPage.tsx
@@ -1,10 +1,17 @@
+import { BikeClient } from "../../api/BikesClient";
 import BikeForm from "../../components/BikeForm/BikeForm";
+import { BikeFormData } from "../../types";
 
 const BikesFormPage = (): React.ReactElement => {
+  const createBike = async (bikeForm: BikeFormData) => {
+    const bikeClient = new BikeClient();
+
+    await bikeClient.createBike(bikeForm);
+  };
   return (
     <>
       <h1 className="page-container__title">Crear una bici</h1>
-      <BikeForm />
+      <BikeForm submitBike={createBike} />
     </>
   );
 };
