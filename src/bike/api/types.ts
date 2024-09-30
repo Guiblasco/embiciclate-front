6349678791e4064c6d09cb78590fb8e78d4b18a7
Guillem Blasco <gowet92@gmail.com>
2024-09-30 21:36:41 +0200

    Add deleteBikeById method at client

--- src/bike/api/types.ts
+++ src/bike/api/types.ts
@@ -3,4 +3,5 @@ import { BikeFormData } from "../types";
 export interface BikeClientStructure {
   getBikes: () => void;
   createBike: (bikeFormData: BikeFormData) => void;
+  deleteBikeById: (bikeId: string) => void;
 }
